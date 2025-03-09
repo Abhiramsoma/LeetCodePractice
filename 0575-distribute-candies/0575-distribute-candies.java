9b88752eb048311a732bb13f990fb7b83550d21f
Abhiramsoma <114901769+Abhiramsoma@users.noreply.github.com>
2025-03-09 23:55:26 +0530

    Time: 19 ms (96%), Space: 46 MB (30.87%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet<Integer> types = new HashSet<>();
+        int canEat = candyType.length / 2;
+        for(int i:candyType){
+            if(types.size()>canEat){
+                return canEat;
+            }
+            types.add(i);
+        }
+        return Math.min(canEat, types.size());
+    }
+}
